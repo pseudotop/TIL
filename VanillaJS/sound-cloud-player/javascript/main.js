de73b2b96a112c9ea7ba860e31f4afdda94fd481
pseudotop <lsupertopl@gmail.com>
2018-11-06 15:16:14 +0900

    Finish making function

--- VanillaJS/sound-cloud-player/javascript/main.js
+++ VanillaJS/sound-cloud-player/javascript/main.js
@@ -1,4 +1,25 @@
 /* 1. 검색 */
+const sidebar = document.querySelector('#js-playlist');
+sidebar.innerHTML = localStorage.getItem('playlist');
+
+const inputArea = document.querySelector(".js-search");
+const searchButton = document.querySelector(".js-submit");
+const resetButton = document.querySelector("#js-reset");
+
+inputArea.addEventListener('keyup', (evt) => {
+  if (evt.which === 13) {
+    const inputValue = document.querySelector('.js-search').value;
+    SoundCloudAPI.getTrack(inputValue);
+  }
+});
+searchButton.addEventListener('click', () => {
+  const inputValue = document.querySelector('.js-search').value;
+  SoundCloudAPI.getTrack(inputValue);
+});
+resetButton.addEventListener('click', () => {
+  localStorage.clear();
+  sidebar.innerHTML = localStorage.getItem('playlist');
+})
 
 /* 2. SoundCloud API  사용하기 */
 const SoundCloudAPI = {
@@ -16,20 +37,15 @@ const SoundCloudAPI = {
       console.log(tracks);
     });
   },
-  getEmbed: (inputAddr) => {
-    SC.oEmbed(inputAddr, {
-      auto_play: true
-    }).then(function (embed){
-      console.log(embed);
-    })
-  }
 };
 
 SoundCloudAPI.init();
-SoundCloudAPI.getTrack('busker');
+
 
 /* 3. 카드 보여주기 */
 SoundCloudAPI.renderTracks = (tracks) => {
+  const searchResults = document.querySelector('#js-search-results');
+  searchResults.innerHTML = null;
   tracks.forEach(track => {
     // Card
     const card = document.createElement('div');
@@ -62,6 +78,9 @@ SoundCloudAPI.renderTracks = (tracks) => {
     // Button
     const button = document.createElement('div');
     button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+    button.addEventListener('click', () => {
+      SoundCloudAPI.addPlaylist(track.permalink_url);
+    });
     const icon = document.createElement('i');
     icon.classList.add('add', 'icon');
     const span = document.createElement('span');
@@ -74,9 +93,25 @@ SoundCloudAPI.renderTracks = (tracks) => {
     card.appendChild(content);
     card.appendChild(button);
 
-    const searchResults = document.querySelector('#js-search-results');
     searchResults.appendChild(card);
   });
 };
 
 /* 4. Playlist 에 추가하고 실제로 재생하기 */
+SoundCloudAPI.addPlaylist = (trackURL) => {
+  SC.oEmbed(trackURL, {
+    auto_play: true
+  }).then(function (embed){
+    // console.log(embed);
+    const sidebar = document.querySelector('#js-playlist');
+    const playbox = document.createElement('div');
+    playbox.classList.add('inner');
+    playbox.innerHTML = embed.html;
+
+    sidebar.insertBefore(playbox, sidebar.firstChild);
+
+    // local storage
+    localStorage.setItem('playlist',playbox.innerHTML);
+    console.log(localStorage);
+  });
+};
