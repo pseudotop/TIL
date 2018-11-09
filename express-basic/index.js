71a56363f12fd0ebb678bcb131b027ccd228cd15
pseudotop <lsupertopl@gmail.com>
2018-11-09 09:45:17 +0900

    Update express-basic

---
+++ express-basic/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Happy Hacking');
+});
+
+app.get('/:name', (req, res) => {
+  res.send(`Hi, ${req.params.name}`);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
