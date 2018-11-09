a8dcc4211109357b2bc72d525583e64bad94dab1
pseudotop <lsupertopl@gmail.com>
2018-11-09 14:35:53 +0900

    Refactoring code lines

--- express-basic/index.js
+++ express-basic/index.js
@@ -28,62 +28,72 @@ app.get('/api/movies', (req, res) => {
 
 /* GET /api/movies/1 */
 app.get('/api/movies/:id', (req, res) => {
-  const movie = movies.find(movie => parseInt(req.params.id) === movie.id);
+  const movie = getMovie(movies, req.params.id);
   if (!movie) {
     res.status(404).send(`Movie with given id(${req.params.id}) is not found`);
   }
-  console.log(movie);
   res.send(movie);
 });
 
 /* POST /api/movies */
 app.post('/api/movies', (req, res) => {
-  const schema = {
-    title: Joi.string().min(2).required(),
-  };
+  const { error } = validateMovie(req.body);
+  if (error) return res.status(400).send(error.message);
   
   const movie = {
     id: movies.length + 1,
     title: req.body.title
   };
-  const result = Joi.validate(req.body, schema);
-  if (result.error) {
-    res.status(400).send(result.error.message);
-  } else {
-    movies.push(movie);
-  }
+  movies.push(movie);
+
   res.send(movie);
 });
 
 /* PUT /api/movies/1 */
-app.put('/api/movies/:id', (req, res) => {
-  const reqSchema = {
-    id: Joi.number().required(),
-  };
-  const bodySchema = {
-    title: Joi.string().min(2).required(),
-  };
+// app.put('/api/movies/:id', (req, res) => {
+//   const reqSchema = {
+//     id: Joi.number().required(),
+//   };
+//   const bodySchema = {
+//     title: Joi.string().min(2).required(),
+//   };
   
-  const reqResult = Joi.validate(req.params, reqSchema);
-  const bodyResult = Joi.validate(req.body, bodySchema);
-  if (reqResult.error || bodyResult.error) {
-    if (reqResult.error) res.status(400).send(reqResult.error.message);
-    if (bodyResult.error) res.status(400).send(bodyResult.error.message);
-  } else {
-    const movie = movies.find((movie) => {
-      if (movie.id === parseInt(req.params.id)) {
-        movie.title = req.body.title;
-        return true;
-      } else {
-        return false;
-      }
-    });
-    if (!movie) {
-      res.status(404).send(`id ${req.params.id} is not exist.`);
-    } else {
-      res.send(`id ${movie.id} is modified.`);
-    }
-  };
+//   const reqResult = Joi.validate(req.params, reqSchema);
+//   const bodyResult = Joi.validate(req.body, bodySchema);
+//   if (reqResult.error || bodyResult.error) {
+//     if (reqResult.error) res.status(400).send(reqResult.error.message);
+//     if (bodyResult.error) res.status(400).send(bodyResult.error.message);
+//   } else {
+//     const movie = movies.find((movie) => {
+//       if (movie.id === parseInt(req.params.id)) {
+//         movie.title = req.body.title;
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     });
+//     if (!movie) {
+//       res.status(404).send(`id ${req.params.id} is not exist.`);
+//     } else {
+//       res.send(`id ${movie.id} is modified.`);
+//     }
+//   };
+// });
+app.put('/api/movies/:id', (req, res) => {
+  // movies id 확인
+  const movie = getMovie(movies, req.params.id);
+  // 없으면 404
+  if(!movie) return res.status(404).send(`id ${movie.id} is not exist`);
+
+  // body 검증
+  // const result = validateMovie(req.body);
+  
+  const { error } = validateMovie(req.body);
+  if (error) return res.status(400).send(error.message);
+  
+  // 데이터 send
+  movie.title = res.body.title; 
+  res.send(movie);
 });
 
 /* DELETE /api/movies/1 */
@@ -109,7 +119,7 @@ app.put('/api/movies/:id', (req, res) => {
 // });
 app.delete('/api/movies/:id', (req, res) => {
   // movies 에서 id 로 movie 찾기
-  const movie = movies.find( movie => movie.id === parseInt(req.params.id));
+  const movie = getMovie(movies, req.params.id);
   // 없으면 404
   if (!movie) return res.status(404).send(`The movie with the given ID is not exist`);
 
@@ -120,6 +130,17 @@ app.delete('/api/movies/:id', (req, res) => {
   // 삭제된 data send
   res.send(movie);
 })
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string().min(2).required(),
+  }
+  return Joi.validate(movie, schema);
+}
+
+function getMovie(movies, id) {
+  return movies.find( movie => movie.id === eval(id));
+}
 
 const port = process.env.PORT || 3000;
 
